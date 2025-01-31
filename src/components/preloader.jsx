75cdbf5c7e761a96b28e8d2dd8a5a5d483bfe386
Gashaw Kidane <Gashaw Kidanu>
2025-01-31 21:41:44 +0300

    preloader customized

--- src/components/preloader.jsx
+++ src/components/preloader.jsx
@@ -1,26 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-
-// const Preloader = () => {
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   useEffect(() => {
-//     // Simulate loading completion
-//     const timer = setTimeout(() => {
-//       setIsLoading(false);
-//     }, 2000); // Adjust timing to match your actual content load
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   return (
-//     <div className={`preloader ${!isLoading ? 'preloader--hidden' : ''}`}>
-//       <div className="preloader__spinner"></div>
-//     </div>
-//   );
-// };
-
-// export default Preloader;
-
 import React, { useState, useEffect } from 'react';
 import './Preloader.css';
 
@@ -37,10 +14,10 @@ const Preloader = () => {
   }, []);
 
   return (
-    <div className={`ek-preloader ${!isLoading ? 'ek-preloader--hidden' : ''}`}>
-      <div className="ek-container">
-        <div className="ek-symbol">△E K</div>
-        <div className="ek-spinner"></div>
+    <div className={`gk-preloader ${!isLoading ? 'gk-preloader--hidden' : ''}`}>
+      <div className="gk-container">
+        <div className="gk-symbol">🧑‍💻GK</div>
+        <div className="gk-spinner"></div>
       </div>
     </div>
   );
